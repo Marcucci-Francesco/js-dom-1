e00f5adeab8596518020c21d1e6c3a3827452205
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-24 17:52:43 +0100

    Exercise completed

---
+++ script.js
@@ -0,0 +1,12 @@
+const lightOff = document.getElementById('light-off');
+const lightOn = document.getElementById('light-on');
+const button = document.querySelector('button');
+
+button.addEventListener ('click', () => {
+  button.classList.toggle('btn-danger');
+  lightOff.classList.toggle('d-none');
+  lightOn.classList.toggle('d-none')
+  lightOn.classList.toggle('d-block');
+  button.innerHTML = button.classList.contains('btn-danger') ? 'Spengi' : 'accendi';
+})
+
